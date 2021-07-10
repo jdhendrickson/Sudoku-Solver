c470b7895e646a5fef5938e0c00e3add1346a413
Joey <Joey@DESKTOP-G03B29I>
2021-07-10 11:25:08 -0700

    Initial Commit

---
+++ main.java
@@ -0,0 +1,23 @@
+import import org.json.simple.JSONObject;
+class SudokuSovler {
+    private size;
+    private cellArray;
+   public static void main(String[] args) {
+
+   }
+   public static SendToJson() {
+
+   }
+   /**
+   * A function to grab information from the json file. Defaults to input.json as filename.
+   */
+   public static ImportFromJson() {
+       public static ImportFromJson("input.json");
+   }
+   /**
+   * A function to grab information from the json file. Defaults to input.json as filename.
+   */
+   public static ImportFromJson(String fileName) {
+       
+   }
+}
