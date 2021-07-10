059e9bcff7683eac31305d2f9489b2fe44ddd5d4
Joey <Joey@DESKTOP-G03B29I>
2021-07-10 11:31:25 -0700

    Added json rules to comment in import file

--- main.java
+++ main.java
@@ -15,9 +15,13 @@ class SudokuSovler {
        public static ImportFromJson("input.json");
    }
    /**
-   * A function to grab information from the json file. Defaults to input.json as filename.
+   * A function to grab information from the json file.
+   * Data in the json file will be stored as a 'size' value that tells you how big the puzzle is.
+   * There will also be a 2-d array holding the starting value stored in each cell.
+   * A "0" in the cell will be interpreted as a blank value.
+   * Values shall be stored as "1" to "9", and then "A" to "Z". More then 36 values will not be supported.
    */
    public static ImportFromJson(String fileName) {
-       
+
    }
 }
