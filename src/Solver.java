119597b1e9c9f0850de478504ff2b7789cf9099c
Joey Hendrickson <jodahendrickson@gmail.com>
2021-10-07 14:55:59 -0700

    Set up start of Solver algorithm

---
+++ src/Solver.java
@@ -0,0 +1,17 @@
+public class Solver {
+    Board board;
+    public Solver() {
+        board = new Board();
+    }
+    public Solver(String in) {
+        board = new Board(in);
+    }
+
+    /**
+     * Solves the sudoku using Brute Force.
+     * Warning: Uses a lot of processing power and time
+     */
+    public void solveBruteForce() {
+
+    }
+}
