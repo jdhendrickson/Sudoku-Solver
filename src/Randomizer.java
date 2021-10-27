80a92abbdbd630c68b209e2cee49ea09dd49aa4d
Joey Hendrickson <jodahendrickson@gmail.com>
2021-10-27 14:44:52 -0700

    Started on basics of creating a new randomized board

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Randomizer {
     private Board board;
     public Randomizer() {
@@ -5,5 +7,41 @@ public class Randomizer {
     }
     public Randomizer(Board in) {
         board = in;
+    }
+
+    /**
+     * Randomize the board
+     * @return a randomized board
+     */
+    public Board randomize() {
+        Random r = new Random();
+        //Clear the current board
+        board.blankBoard();
+        //Create a random solution
+        ///Add a random value for each number
+        int x, y;
+        Cell tempCell;
+        for (int i = 0; i < board.getSize(); i++) {
+            x = r.nextInt(board.getSize() + 1);
+            y = r.nextInt(board.getSize() + 1);
+            if (board.getCell(x,y).getContent() == '0') {
+                //Blank cell, can be used
+                board.getCell(x,y).setContent(Helpers.iterToChar((char) i));
+            }
+        }
+
+
+        //Set cells to be starters
+        for (int i = 0; i < board.getSize(); i++) {
+            for (int j = 0; j < board.getSize(); j++) {
+                if (board.getCell(i,j).getContent() != '0') {
+                    //This is not a blank cell, and is thus a starter
+                    tempCell = new Cell(true);
+                    tempCell.setContent(board.getCell(i,j).getContent());
+                    board.setCell(i,j,tempCell);
+                }
+            }
+        }
+        return board;
     }
 }
